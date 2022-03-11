a36f7927600797b2e5f8aee761a057103b026f4e
ramneek <42456883+rkriar@users.noreply.github.com>
2022-03-10 20:25:25 -0500

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,13 @@
+"""
+Helper functions live here
+"""
+
+
+def get_area_of_rectangle(width: int, height: int):
+    """
+    This function finds the area of a rectangle.
+    :param width: the width of the rectangle
+    :param height: the height of the rectangle
+    :return: the area of the rectangle
+    """
+    return width * height
